ffb941012cc09b6ef52eea0dc61a59256445365a
Oskar Makowski <116796054+Kedjian@users.noreply.github.com>
2023-06-02 22:49:23 +0200

    Initial commit

---
+++ simpleover/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int sum(int num1, int num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+short sum(short num1, short num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+float sum(float num1, float num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+double sum(double num1, double num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+long sum(long num1, long num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+long double sum(long double num1, long double num2) {
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl << endl;
+    return 0;
+}
+
+int main() {
+    sum((int)2, (int)3);
+    sum((float)3.5, (float)6.2);
+    sum((double)7.662, (double)9.221);
+    sum((short)16380, (short)16380);
+    sum((long)-9223372036854775, (long)2233720368521);
+    sum((long double)-233434.56343, (long double)230021.56334);
+}
